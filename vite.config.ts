1a493fede5ea1c71869073a67e5e436d48ed8baa
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-07-10 14:31:23 +0530

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DhrubDubey_LetsUpgradeFrontendSprint/',
+  base: process.env.VITE_APP_BASE_PATH || '/DhrubDubey_LetsUpgradeFrontendSprint/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
