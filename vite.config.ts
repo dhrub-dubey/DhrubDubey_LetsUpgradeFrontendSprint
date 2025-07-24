03cec0f79b0cd17417a3c6064a0cbff4974a87a5
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-07-24 22:24:07 +0530

    Updated vite.config for GitHub Pages and Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_APP_BASE_PATH || '/DhrubDubey_LetsUpgradeFrontendSprint/',
+  base:  "cross-env VITE_APP_BASE_PATH=/DhrubDubey_LetsUpgradeFrontendSprint/ vite build && gh-pages -d dist",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
