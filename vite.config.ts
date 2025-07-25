9d2acbd95de0e9650be509ae2b57b1fd2b98827b
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-07-25 19:41:35 +0530

    fix vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:  isGitHubPages ? '/DhrubDubey_LetsUpgradeFrontendSprint/' : '/',
+  base:  isGitHubPages ? '/DhrubDubey_LetsUpgradeFrontendSprint/' : './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
